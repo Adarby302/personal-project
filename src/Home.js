365f9752bc27751b41e782ac75c8df6895e3e07e
alex <dandarby228@gmail.com>
2025-08-06 10:43:10 -0400

    grammar corrections

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
         <div className="intro-text">
           <h1 className="intro-title animate-slideup">Hello there!</h1>
           <p style={{textAlign: 'justify', width: '100%'}}>
-              What you will find here is a niche assortment of some projects I've created over time
+              What you will find here is a niche assortment of some projects I've created
               in my free time. I have a large interest in a variety of technologies and
               dive into things that interest me during those moments in time.<br />
           </p>
