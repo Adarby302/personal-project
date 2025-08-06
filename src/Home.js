6c86bb68a5a490749b3e8def28546489341c5fde
Alex <dandarby228@gmail.com>
2025-08-06 01:39:51 -0400

    fixed grammar issues here

--- src/Home.js
+++ src/Home.js
@@ -8,9 +8,9 @@ const Home = () => {
         <div className="intro-text">
           <h1 className="intro-title animate-slideup">Hello there!</h1>
           <p style={{textAlign: 'justify', width: '100%'}}>
-              What you will find here is a niche assortment of some projects I've created overtime<br />
-              on my freetime. I personally have a large intrest in a variety of technologies and <br />
-              dive into things that intrest me during those moments in time.<br />
+              What you will find here is a niche assortment of some projects I've created over time
+              in my free time. I have a large interest in a variety of technologies and
+              dive into things that interest me during those moments in time.<br />
           </p>
           <p> <em>Website is still work in progress 🔧</em></p>
           <Link to="/projects" className="btn-primary">View My Work</Link>
